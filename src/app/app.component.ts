2d0e4662e570a2274c7276eac4fb7fd6e5bbd2e9
kaushal1983 <kaushalkishore.mca@gmail.com>
2020-03-30 17:36:51 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  formdata;
+  todoList =[
+
+{'title':'title 1', 'desc':'description 1', 'start': '13/02/2020', 'end': '13/02/2020', 'status': 'Done'},
+
+{'title':'title 2', 'desc':'description 2', 'start': '13/02/2020', 'end': '20/02/2020', 'status': 'WIP'}
+
+];
+
+ngOnInit() {
+     
+      this.formdata = new FormGroup({
+         emailid: new FormControl("angular@gmail.com"),
+         passwd: new FormControl("abcd1234")
+      });
+   }
+
 }
