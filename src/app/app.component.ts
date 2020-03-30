a60ef44993d60fa26492625cf9372a06ee17dfcd
kaushal1983 <kaushalkishore.mca@gmail.com>
2020-03-30 17:48:17 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent  {
   formdata;
+  title;
+  desc;
+  startDate;
+  endDate;
+  status;
   todoList =[
-
 {'title':'title 1', 'desc':'description 1', 'start': '13/02/2020', 'end': '13/02/2020', 'status': 'Done'},
 
 {'title':'title 2', 'desc':'description 2', 'start': '13/02/2020', 'end': '20/02/2020', 'status': 'WIP'}
@@ -22,5 +26,5 @@ ngOnInit() {
          passwd: new FormControl("abcd1234")
       });
    }
-
+   onClickSubmit(data) {this.emailid = data.emailid;}
 }
